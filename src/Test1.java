7f40d7e84ac1c9a8a00d9d424e02a22681c38ace
Luong-Code <zonina123@gmail.com>
2023-10-05 21:05:49 +0800

    Test

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Test1 {
+public class Test1
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+    }
+
+}
+
+// for class human, to remember the constructor
+class Human
+{
+    private String name;
+    private int age;
+    private String gender;
+
+    public Human(String name, int age, String gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
 }
