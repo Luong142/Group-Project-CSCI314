6bcaf74c26b73143798b25de6aa3bc6e9622e42b
Luong-Code <zonina123@gmail.com>
2023-10-09 14:14:16 +0800

    Boilerplates establishment
    UserAccount and UserProfile has been formed in basic manner

--- src/EntityClass/UserAccount.java
+++ src/EntityClass/UserAccount.java
@@ -1,4 +1,46 @@
 package EntityClass;
 
-public class UserAccount {
+public class UserAccount
+{
+    private String username;
+    private String password;
+    private String role;
+
+    public UserAccount(String username, String password, String role)
+    {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    void setRole(String role)
+    {
+        this.role = role;
+    }
+
+    String getUsername()
+    {
+        return username;
+    }
+
+    String getPassword()
+    {
+        return password;
+    }
+
+    String getRole()
+    {
+      return role;
+
+    }
 }
--- src/EntityClass/UserProfile.java
+++ src/EntityClass/UserProfile.java
@@ -2,6 +2,64 @@ package EntityClass;
 
 public class UserProfile
 {
+    private String name;
+    private int age;
+    private String gender;
+    private int phone;
+    private String email;
 
+    public UserProfile(String name, int age, String gender, int phone, String email)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+    }
 
+    // setter
+    void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    void setName(String name)
+    {
+        this.name = name;
+    }
+
+    void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    void setPhone(int phone)
+    {
+        this.phone = phone;
+    }
+
+    String getName(String name)
+    {
+        return name;
+    }
+
+    String getGender(String gender)
+    {
+        return gender;
+    }
+
+    String getEmail(String email)
+    {
+        return email;
+    }
+
+    int getAge(int age)
+    {
+        return age;
+    }
 }
