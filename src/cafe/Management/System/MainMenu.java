84fb9089d27f65c858da25ac19088dffbea2de06
Luong-Code <zonina123@gmail.com>
2023-10-16 14:38:38 +0800

    Iniitial Log in Page
    haven't settled with the class

--- src/cafe/Management/System/MainMenu.java
+++
@@ -1,9 +0,0 @@
-package Cafe.Management.System;
-
-import javax.swing.*;
-
-public class MainMenu
-{
-    private JPanel jPanelMain;
-    private JLabel Name;
-}
---
+++ src/cafe/Management/System/SignUp.java
@@ -0,0 +1,23 @@
+package Cafe.Management.System;
+
+import javax.swing.*;
+
+public class SignUp
+{
+    private JPanel jPanelSignUp;
+    private JLabel cafeLabel;
+    private JLabel userName;
+    private JTextField userNameInput;
+    private JPasswordField passWordInput;
+    private JButton logInButton;
+    private JButton forgetPasswordButton;
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
+
+
+
